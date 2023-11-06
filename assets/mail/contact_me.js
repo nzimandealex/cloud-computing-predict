4a12c3add2cd51bcfb429c17150eba6a15a5e3a6
nzimandepa@gmail.com <nzimandepa@gmail.com>
2023-11-06 20:34:02 +0200

    vreated dynamo, and fixed contact me

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://gqovu1om17.execute-api.eu-west-1.amazonaws.coms://gqovu1om17.execute-api.eu-west-1.amazonaws.com",
+                url: "https://gqovu1om17.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
